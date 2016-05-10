8d66d5acffbf48ac1c923c881db919678387fc42
pjgg <pablo.gonzalez.granados@gmail.com>
2016-05-10 15:12:00 +0200

    config from plainFile

--- src/main/scala/org/radioactiveMongoTemplate/dao/MongoContext.scala
+++ src/main/scala/org/radioactiveMongoTemplate/dao/MongoContext.scala
@@ -1,29 +1,77 @@
 package org.radioactiveMongoTemplate.dao
 
+import com.typesafe.config.ConfigFactory
 import reactivemongo.api.commands._
-import reactivemongo.api.{ReadPreference, CrAuthentication, MongoConnectionOptions, MongoDriver}
+import reactivemongo.api._
+import scala.collection.JavaConversions._
+
 
 
 object MongoContext {
 
   private val driver = new MongoDriver
+  private val conf = ConfigFactory.load()
 
   val connectionOptions = MongoConnectionOptions(
-    0,
-    None,
-    false,
-    false,
-    CrAuthentication,
-    false,
-    false,
-    10,
-    WriteConcern.Default,
-    ReadPreference.primary
+    conf.getInt("connectTimeoutMS"),
+    Option(conf.getString("authSource")),
+    conf.getBoolean("sslEnabled"),
+    conf.getBoolean("sslAllowsInvalidCert"),
+    getAuthMethod,
+    conf.getBoolean("tcpNoDelay"),
+    conf.getBoolean("keepAlive"),
+    conf.getInt("nbChannelsPerNode"),
+    getWriteConcern,
+    getReadPreference
   )
 
-   val connection = driver.connection(List("localhost:27017"), options =  connectionOptions)
+   val connection = driver.connection(nodes = getHosts, options =  connectionOptions)
 
-   val maxBulkSize = 1000
+   val maxBulkSize = getMaxBulkSize().getOrElse(1000)
 
-   val maxBsonSize = 6000000
+   val maxBsonSize = getMaxBsonSize().getOrElse(6000000)
+
+  private def getAuthMethod():AuthenticationMode={
+    val auth:String = conf.getString("authMode")
+    auth match {
+      case "ScramSha1Authentication" => ScramSha1Authentication
+      case _ => CrAuthentication
+    }
+  }
+
+  private def getWriteConcern():GetLastError = {
+    val writeConcern:String = conf.getString("writeConcern")
+    writeConcern match {
+      case "Acknowledged" => WriteConcern.Acknowledged
+      case "Journaled" => WriteConcern.Journaled
+      case "Unacknowledged" =>  WriteConcern.Unacknowledged
+      case _ =>  WriteConcern.Default
+    }
+  }
+
+  private def getReadPreference():ReadPreference={
+    val readPreference:String = conf.getString("readPreference")
+    readPreference match {
+      case "primaryPreferred" => ReadPreference.primaryPreferred
+      case "secondary" => ReadPreference.secondary
+      case "secondaryPreferred" =>  ReadPreference.secondaryPreferred
+      case "nearest" =>  ReadPreference.nearest
+      case _ =>  ReadPreference.primary
+    }
+  }
+
+  private def getMaxBulkSize():Option[Int] = {
+    val maxBulkSize = conf.getInt("maxBulkSize")
+    if(maxBulkSize == 0) None else Some(maxBulkSize)
+  }
+
+  private def getMaxBsonSize():Option[Int] = {
+    val maxBsonSize = conf.getInt("maxBsonSize")
+    if(maxBsonSize == 0) None else Some(maxBsonSize)
+  }
+
+  private def getHosts(): java.util.List[String] = {
+    val l = conf.getStringList("hosts").toList
+    if(l.isEmpty) List("localhost:27017") else l
+  }
 }
